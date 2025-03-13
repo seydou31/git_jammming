76e9f01c0b001db8a416ed8301da4bec3b58aa73
seydou <badiakaseydou@gmail.com>
2025-03-13 06:08:52 -0400

    Added search bar component

---
+++ src/SearchBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SearchBar(){
+    return (
+    <div className='SearchBar'>
+       <form>
+        <input type='text' value=''/>
+        <button type='submit'>Search</button>
+       </form>
+    </div>)
+}
