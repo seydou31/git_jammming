039effa50a261da0e4953f64dd7f67555459726e
seydou <badiakaseydou@gmail.com>
2025-04-11 13:11:58 -0400

    Get access token

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import './App.css';
 
+const CLIENT_ID = "162c98f0ddd84abcb371b1ba703fee55";
+  const CLIENT_SERVER = "28a0cc6ed05f4b66ba1a6962b8a10fd9";
+
+
 function App() {
+  useEffect(() => {
+    var authParameters = {
+      method : 'Post',
+      header : {
+        'content-type' : 'application/x-www-form-urlencoded'
+      },
+      body : 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SERVER
+    }
+    fetch('https://accounts.spotify.com/api/token', authParameters)
+    .then(response => response.json())
+    .then(data => console.log(data))
+
+  }, [])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, World</h1>
     </div>
   );
 }
